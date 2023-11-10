8511fdaf06783241c873229e8098ab2a22036bed
LethalGerard <lethal_kim@hotmail.com>
2023-11-10 14:10:56 +0100

    Motorcycles va ignored i VS och ska nu följa med

---
+++ CarRentalVG.Common/Classes/Motorcycles.cs
@@ -0,0 +1,11 @@
+﻿using CarRentalVG.Common.Enums;
+using CarRentalVG.Common.Interfaces;
+
+namespace CarRentalVG.Common.Classes;
+
+public class Motorcycles : VehicleInherit
+{
+    public Motorcycles(int id,string regNo, string make, int odometer, double costKm, VehicleTypes vehicleType, double costday)
+        : base(id, regNo, make, odometer, costKm, vehicleType, costday)
+    { }
+}
